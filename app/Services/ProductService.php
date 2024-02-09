1f50bc1cf1393095dc517727ee64340e04a373aa
sjovicevic <jovicevicst@icloud.com>
2024-02-09 15:24:55 +0100

    added productservice

---
+++ app/Services/ProductService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\ProductRepository;
+
+class ProductService
+{
+    protected ProductRepository $productRepository;
+
+    public function __construct() {
+        $this->productRepository = new ProductRepository();
+    }
+
+    public function getAllProductsByCategory(int $product_category_id) {
+        return $this->productRepository->getAllProductsByCategory($product_category_id);
+    }
+}
