4fbfce8c109016271a339266285d1818b29dc634
Janko Blagojevic <janko.tbbt@gmail.com>
2024-07-02 01:39:20 +0200

    phase 3 init

--- resources/views/products/categories.blade.php
+++ resources/views/products/categories.blade.php
@@ -4,7 +4,7 @@
 
     <section class="single-category-section cookies">
         <div class="container">
-            <img src="{{ asset('images/dodaci-za-kolace-m.png') }}" alt="" class="mobile product-mobile">
+            <img src="{{ asset('images/dodaci-za-kolace-hero-mobile.png') }}" alt="" class="mobile product-mobile">
             <div class="single-category-inner">
                 <h2>Dodaci za kolače</h2>
                 <p>Dodaci koji su generacijama unazad neizostavni saveznici u kuhinji. Za kolače ili peciva, za posebne trenutke ili svakodnevna uživanja, C Slatka tradicija dodaci za kolače su tajna ukusa kojima se cela porodica raduje. Nastavi slatku tradiciju sa našim dodacima za kolače i torte. Otkrij tajnu savršenog ukusa.</p>
@@ -15,7 +15,7 @@
 
     <section class="single-category-section puding">
         <div class="container">
-            <img src="{{ asset('images/puding-m.png') }}" alt="" class="mobile product-mobile">
+            <img src="{{ asset('images/puding-hero-mobile.png') }}" alt="" class="mobile product-mobile">
             <div class="single-category-inner">
                 <h2>Puding</h2>
                 <p>Da li ćeš ga spremiti kao samostalnu poslasticu ili dodati u kolače i torte? Svejedno je! Puding će ti vratiti osmeh na lice. Odaberi tvoj omiljeni ukus C Slatka tradicija pudinga. U njemu uživa cela porodica, a za njegovu pripremu nije potrebno mnogo vremena.</p>
@@ -26,7 +26,7 @@
 
     <section class="single-category-section slag">
         <div class="container">
-            <img src="{{ asset('images/slag-m.png') }}" alt="" class="mobile product-mobile">
+            <img src="{{ asset('images/slag-hero-mobile.png') }}" alt="" class="mobile product-mobile">
             <div class="single-category-inner">
                 <h2>Šlag i Šlag krem</h2>
                 <p>Decenijama unazad, tajni saveznik neodoljivih torti, sočnih kolača i laganih voćnih kupova, C Šlag i C Šlag krem svaku poslasticu čine još ukusnijom i potpuno neodoljivom. Mogu se koristiti kao dekoracija na tortama, ali i kao ukusni krem u omiljenim desertima.</p>
@@ -59,7 +59,7 @@
 
     <section class="single-category-section eskimko">
         <div class="container">
-            <img src="{{ asset('images/eskimko-m.png') }}" alt="" class="mobile product-mobile">
+            <img src="{{ asset('images/eskimko-hero-mobile.png') }}" alt="" class="mobile product-mobile">
             <div class="single-category-inner">
                 <h2>Eskimko</h2>
                 <p><strong>Leden, ukusan i osvežavajući.</strong> Lagana i brza priprema i neodoljivi ukusi, čine da svaki dan bude ledeno sladak. Možeš ga koristiti u pripremi omiljenih torti i kolača, ali i uz topinge ili voće po želji servirati najukusniji sladoled.</p>
@@ -70,7 +70,7 @@
 
     <section class="single-category-section zimnica">
         <div class="container">
-            <img src="{{ asset('images/zimnica-m.png') }}" alt="" class="mobile product-mobile">
+            <img src="{{ asset('images/zimnica-hero-mobile.png') }}" alt="" class="mobile product-mobile">
             <div class="single-category-inner">
                 <h2>Zimnica</h2>
                 <p>C Slatka tradicija proizvodi su tu da ti budu desna ruka dok pripremate turšije, kisele salate, sokove, marmelade i pekmeze. Vinobran, limuntus ili konzervans pomoći će ti da omiljene ukuse sačuvate za hladne, zimske dane.</p>
