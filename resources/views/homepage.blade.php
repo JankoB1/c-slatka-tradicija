2ecb7906c094ac321e7ddd7518899b6a43762a0a
Janko Blagojevic <janko.tbbt@gmail.com>
2024-07-29 10:06:07 +0300

    teskt

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -87,7 +87,7 @@
             <div class="row">
                 <div class="col-md-6">
                     <h3>Tvoj omiljeni obrok <br>još od detinjstva</h3>
-                    <p>Za stare i mlade, za one koji vole ukuse detinjstva.</p>
+                    <p>Za stare i mlade, za one koji vole ukuse detinjstva. Dečak sa pakovanja je dobro poznat svima i generacijama unazad je deo našeg odrastanja.</p>
                     <a href="{{ route('show-single-product', ['slug' => 'psenicni-griz-200g']) }}">Saznaj više</a>
                 </div>
             </div>
